bff33d5cb6763778980cab675ee742b2491f84ed
Denis Yagofarov <denyago@gmail.com>
2012-08-30 22:00:17 +0300

    Add DB migrations setup - first try

--- lib/rack/oauth2/models/active_record.rb
+++ lib/rack/oauth2/models/active_record.rb
@@ -3,8 +3,10 @@ module Rack
     class Server
 
       class ActiveRecord < ::ActiveRecord::Base
-        set_table_name do
-          "oauth2_provider_#{name.split("::").last.underscore}"
+        TABLE_PREFIX = "oauth2_provider_"
+
+        def self.table_name
+          TABLE_PREFIX + name.split("::").last.underscore
         end
       end
 
@@ -14,7 +16,41 @@ module Rack
           instance = klass.new fields
         end
       end
+      class CreateRackOauth2ServerSchema < ::ActiveRecord::Migration
+        def change
 
+          create_table "#{ActiveRecord::TABLE_PREFIX}client" do |t|
+            # Client identifier.
+            t.string :client_id
+            # Client secret: random, long, and hexy.
+            t.string :secret
+            # User see this.
+            t.string :display_name
+            # Link to client's Web site.
+            t.string :link
+            # Preferred image URL for this icon.
+            t.string :image_url
+            # Redirect URL. Supplied by the client if they want to restrict redirect URLs (better security).
+            t.string :redirect_uri
+            # List of scope the client is allowed to request.
+            t.string :scope
+            # Free form fields for internal use.
+            t.string :notes
+            # Timestamp if revoked.
+            t.datetime :revoked
+            # Counts how many access tokens were granted.
+            t.integer :tokens_granted
+            # Counts how many access tokens were revoked.
+            t. integer :tokens_revoked
+
+            t.timestamps
+          end
+          change_table "#{ActiveRecord::TABLE_PREFIX}client" do |t|
+            t.index :client_id
+            t.index [:client_id, :secret]
+          end
+        end
+      end
     end
   end
 end
